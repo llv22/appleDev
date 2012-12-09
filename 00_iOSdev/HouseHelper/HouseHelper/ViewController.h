6edb002630576a06fdd8cdc9612772afdfe2df5b
Orlando Ding <xiandao.airs@gmail.com>
2012-12-09 22:13:25 +0800

    llv22 - house position in chengdu Dec

---
+++ 00_iOSdev/HouseHelper/HouseHelper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HouseHelper
+//
+//  Created by Ding Orlando on 12/9/12.
+//  Copyright (c) 2012 Ding Orlando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
