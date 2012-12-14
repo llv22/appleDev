e3c2fd67753e0b92e5d2c94cae0dd450f167f17e
Orlando Ding <xiandao.airs@gmail.com>
2012-12-14 20:12:36 +0800

    llv22 - HouseAnnotation

---
+++ 00_iOSdev/HouseHelper/HouseHelper/HouseBaseAnnotation.h
@@ -0,0 +1,14 @@
+//
+//  HouseBaseAnnotation.h
+//  HouseHelper
+//
+//  Created by Ding Orlando on 12/14/12.
+//  Copyright (c) 2012 Ding Orlando. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface HouseBaseAnnotation : NSObject<MKAnnotation>
+
+@end
---
+++ 00_iOSdev/HouseHelper/HouseHelper/HouseBaseAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  HouseBaseAnnotation.m
+//  HouseHelper
+//
+//  Created by Ding Orlando on 12/14/12.
+//  Copyright (c) 2012 Ding Orlando. All rights reserved.
+//
+
+#import "HouseBaseAnnotation.h"
+
+@implementation HouseBaseAnnotation
+
+@end
