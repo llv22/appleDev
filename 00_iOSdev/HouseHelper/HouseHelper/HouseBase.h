8ec5a73ce9664cff12e0ec0348b30e728e954a73
Orlando Ding <xiandao.airs@gmail.com>
2012-12-14 20:09:05 +0800

    llv22 - housing refactor

---
+++ 00_iOSdev/HouseHelper/HouseHelper/HouseBase.h
@@ -0,0 +1,13 @@
+//
+//  HouseBase.h
+//  HouseHelper
+//
+//  Created by Ding Orlando on 12/14/12.
+//  Copyright (c) 2012 Ding Orlando. All rights reserved.
+//
+
+#import "MetroStop.h"
+
+@interface HouseBase : MetroStop
+
+@end
---
+++ 00_iOSdev/HouseHelper/HouseHelper/HouseBase.m
@@ -0,0 +1,13 @@
+//
+//  HouseBase.m
+//  HouseHelper
+//
+//  Created by Ding Orlando on 12/14/12.
+//  Copyright (c) 2012 Ding Orlando. All rights reserved.
+//
+
+#import "HouseBase.h"
+
+@implementation HouseBase
+
+@end
