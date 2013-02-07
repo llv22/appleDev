7230e32f90d40ec6e0b4ce77bbebcbeb7266d668
Orlando Ding <xiandao.airs@gmail.com>
2013-02-07 18:02:24 +0800

    llv22 - online google books api in iOS application

--- 00_iOSdev/OnlineGoogleBook/OnlineGoogleBook/ViewController.m
+++ 00_iOSdev/OnlineGoogleBook/OnlineGoogleBook/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+- (void) loadHtmlStringLocally;
+- (void) loadHtmlResourceLocally;
+
 @end
 
 @implementation ViewController
@@ -18,13 +21,34 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self loadHtmlStringLocally];
+}
+
+// desc - load html via String in UIWebContainer - not used now
+- (void) loadHtmlResourceLocally{
     NSString* path = [[NSBundle mainBundle]pathForResource:@"iOS6core" ofType:@"html"];
     NSURL* url = [NSURL fileURLWithPath:path];
-//    NSURLRequest* req = [[NSURLRequest alloc]initWithURL:url];
-//    [self->webview loadRequest:req];
+    NSURLRequest* req = [[NSURLRequest alloc]initWithURL:url];
+    [self->webview loadRequest:req];
+}
+
+// desc - load html via String in UIWebContainer - not used now
+// requirements :
+//      1, need to adjust for UIContainer to UI status
+//      2, need to add UI status refreshment
+- (void) loadHtmlStringLocally{
+    NSString* path = [[NSBundle mainBundle]pathForResource:@"iOS6coreTemplate" ofType:@"html"];
+    NSURL* url = [NSURL fileURLWithPath:path];
     NSError* error = nil;
-    NSString* s = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:&error];
-    [self->webview loadHTMLString:s baseURL:url];
+    NSString* strWebContent = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:&error];
+//    NSLog(@"%f, %f", self->webview.frame.size.width, self->webview.frame.size.height);
+    int iViewWidth = self->webview.frame.size.width, iViewHeight = self->webview.frame.size.height;
+    
+    //desc - replace with UI size, http://stackoverflow.com/questions/668228/string-replacement-in-objective-c
+    strWebContent = [strWebContent stringByReplacingOccurrencesOfString:@"$width$" withString:[NSString stringWithFormat:@"%d", iViewWidth]];
+    strWebContent = [strWebContent stringByReplacingOccurrencesOfString:@"$height$" withString:[NSString stringWithFormat:@"%d", iViewHeight]];
+    [self->webview sizeToFit];
+    [self->webview loadHTMLString:strWebContent baseURL:url];
 }
 
 - (void)didReceiveMemoryWarning
