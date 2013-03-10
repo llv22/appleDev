dccb117c9de338e97c16c9d2cdc96adb9743ce65
Orlando Ding <xiandao.airs@gmail.com>
2013-03-10 17:29:36 +0800

    llv22 - new decorationview template

---
+++ 00_iOSdev/OnlineGoogleBooks/CollectionViewExamples/DecorationView/DecorationView/DecorationLayout.m
@@ -0,0 +1,36 @@
+//
+//  DecorationLayout.m
+//  DecorationView
+//
+//  Created by Ding Orlando on 3/10/13.
+//  Copyright (c) 2013 Ding Orlando. All rights reserved.
+//
+//  see sample of
+//  1, http://blogs.captechconsulting.com/blog/paul-dakessian/ios-6-tutorial-getting-started-collection-views
+//  2, http://stackoverflow.com/questions/12810628/uicollectionview-decoration-view
+//  3, http://www.appcoda.com/ios-programming-uicollectionview-tutorial/
+//
+
+#import "DecorationViewCell.h"
+#import "DecorationLayout.h"
+
+@implementation DecorationLayout
+
+-(void)prepareLayout{
+    // desc - dummy register
+    [self registerClass:[DecorationViewCell class] forDecorationViewOfKind:DECORID];
+}
+
+// desc - dummy decoration
+-(NSArray *)layoutAttributesForElementsInRect:(CGRect)rect{
+    return  NULL;
+}
+
+#pragma mark - decoration layout delegate
+// desc - dummy decoration
+-(UICollectionViewLayoutAttributes *)layoutAttributesForDecorationViewOfKind:(NSString *)decorationViewKind
+                                                                 atIndexPath:(NSIndexPath *)indexPath{
+    return NULL;
+}
+
+@end
---
+++ 00_iOSdev/OnlineGoogleBooks/CollectionViewExamples/DecorationView/DecorationView/DecorationViewCell.h
@@ -0,0 +1,18 @@
+//
+//  DecorationViewCell.h
+//  DecorationView
+//
+//  Created by Ding Orlando on 3/10/13.
+//  Copyright (c) 2013 Ding Orlando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// see http://stackoverflow.com/questions/6828831/sending-const-nsstring-to-parameter-of-type-nsstring-discards-qualifier
+#ifndef DECORID
+#define DECORID @"DecorationViewForCell"
+#endif
+
+@interface DecorationViewCell : UICollectionViewCell
+
+@end
