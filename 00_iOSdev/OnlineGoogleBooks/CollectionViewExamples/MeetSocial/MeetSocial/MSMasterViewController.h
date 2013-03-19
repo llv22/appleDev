9a1a063a41d1832ae7c43e4f1f4faff572f85231
Orlando Ding <xiandao.airs@gmail.com>
2013-03-19 22:17:51 +0800

    llv22 - collection view example in ios 6.0 practise

---
+++ 00_iOSdev/OnlineGoogleBooks/CollectionViewExamples/MeetSocial/MeetSocial/MSMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MSMasterViewController.h
+//  MeetSocial
+//
+//  Created by Ding Orlando on 3/19/13.
+//  Copyright (c) 2013 Ding Orlando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MSDetailViewController;
+
+@interface MSMasterViewController : UITableViewController
+
+@property (strong, nonatomic) MSDetailViewController *detailViewController;
+
+@end
---
+++ 00_iOSdev/OnlineGoogleBooks/CollectionViewExamples/MeetSocial/MeetSocial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MeetSocial
+//
+//  Created by Ding Orlando on 3/19/13.
+//  Copyright (c) 2013 Ding Orlando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSAppDelegate class]));
+    }
+}
